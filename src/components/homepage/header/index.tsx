c3ec32e233d1756e3a10e4d2f190d7cf5c94d6de
서진 <dltjwls0529@gmail.com>
2023-08-17 16:29:20 +0900

    💄 홈 페이지 헤더 퍼블리싱

--- src/components/homepage/header/index.tsx
+++ src/components/homepage/header/index.tsx
@@ -1,10 +1,26 @@
 import "./header.scss";
 import { ReactComponent as ChatIcon } from "@assets/homepage/chat.svg";
+import { ReactComponent as NewChatIcon } from "@assets/homepage/new_chat.svg";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Header = () => {
+  const nav = useNavigate();
+  const [newChat, isNewChat] = useState<Boolean>(true);
   return (
     <div className="header-container">
-      <ChatIcon />
+      <div className="left">
+        <div className="header-profile-circle" onClick={() => nav("/mypage")}>
+          <img
+            src="https://c1.wallpaperflare.com/preview/714/489/198/chess-chess-board-game-board-flag.jpg"
+            alt="profile"
+          />
+        </div>
+        <div className="header-title">KUDDY</div>
+      </div>
+      <div className="header-chat-container" onClick={() => nav("/chat")}>
+        {newChat ? <NewChatIcon /> : <ChatIcon />}
+      </div>
     </div>
   );
 };
