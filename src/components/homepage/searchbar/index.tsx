907ad52a153e4db84535c20cc2ff6b1d80fc89f2
서진 <dltjwls0529@gmail.com>
2023-08-17 16:32:35 +0900

    💄 홈 페이지 swiper 카드, 검색 바 퍼블리싱

--- src/components/homepage/searchbar/index.tsx
+++ src/components/homepage/searchbar/index.tsx
@@ -1,7 +1,17 @@
-import React from "react";
+import "./searchbar.scss";
+import { ReactComponent as SearchIcon } from "@assets/icon/search.svg";
+import { useNavigate } from "react-router-dom";
 
 const SearchBar = () => {
-  return <></>;
+  const nav = useNavigate();
+  return (
+    <div className="searchbar-wrapper">
+      <div className="searchbar-box" onClick={() => nav("/travel/search")}>
+        <SearchIcon stroke="var(--color-black)" />
+        <p>Everything for your travel</p>
+      </div>
+    </div>
+  );
 };
 
 export default SearchBar;
--- src/components/homepage/swipercard/index.tsx
+++ src/components/homepage/swipercard/index.tsx
@@ -1,7 +1,64 @@
-import Swiper from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/swiper.scss";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import "./swipercard.scss";
+
+type SwiperInfoType = {
+  id: number;
+  title: string;
+  src: string;
+};
 
 const SwiperCard = () => {
-  return <></>;
+  const nav = useNavigate();
+  const [swiperInfo, setSwiperInfo] = useState<SwiperInfoType[]>([
+    {
+      id: 1,
+      title: "10 The best view point for Han-River",
+      src: "https://me.go.kr/hg/file/preview.do?fileId=187108&fileSeq=1",
+    },
+    {
+      id: 2,
+      title: "The Most Picked Jamsil Spot",
+      src: "https://blog.kakaocdn.net/dn/xIoxp/btrB5V8Gf2a/LMWasLuAC6tkdo8hauzm10/img.jpg",
+    },
+    {
+      id: 3,
+      title: "10 The best view point for Han-River",
+      src: "https://me.go.kr/hg/file/preview.do?fileId=187108&fileSeq=1",
+    },
+  ]);
+  const [swiperIndex, setSwiperIndex] = useState<Number>(0);
+  useEffect(() => {
+    if (swiperIndex === 0 || swiperIndex === swiperInfo.length - 1) {
+    }
+  }, [swiperIndex]);
+  return (
+    <>
+      <Swiper
+        slidesPerView={"auto"}
+        spaceBetween={8}
+        centeredSlides={true}
+        onSlideChange={swiperCore => setSwiperIndex(swiperCore.activeIndex)}
+      >
+        {swiperInfo.map(item => (
+          <SwiperSlide
+            key={item.id}
+            onClick={() => nav(`/kuddys-pick/${item.id}`)}
+          >
+            <div className="swiper-img-rect">
+              <img src={item.src} />
+            </div>
+            <div className="swiper-text-container">
+              <div className="swiper-text small">KUDDY's Pick</div>
+              <div className="swiper-text">{item.title}</div>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </>
+  );
 };
 
 export default SwiperCard;
