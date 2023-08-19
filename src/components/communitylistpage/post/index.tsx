59d3e5bb54de43cb26fac9a1a2883c392f8870a2
Yerineee <yerin1103@ewhain.net>
2023-08-19 17:12:26 +0900

    :lipstick: Community List Page 게시물 퍼블리

---
+++ src/components/communitylistpage/post/index.tsx
@@ -0,0 +1,54 @@
+import "./post.scss";
+import commentIcon from "@assets/community/comment.svg";
+
+type categoryType = "feedback" | "talking";
+
+type courseType = {
+  id: number;
+  name: string;
+};
+
+type PostProps = {
+  type: categoryType;
+  id: number;
+  title: string;
+  content: string;
+  courses?: courseType[];
+  date: string;
+  comment: number;
+};
+
+const Post = ({
+  type,
+  id,
+  title,
+  content,
+  courses,
+  date,
+  comment,
+}: PostProps) => {
+  return (
+    <div className="post-container">
+      <div className="post-title">{title}</div>
+      <div className="post-content">{content}</div>
+      <div className="post-courses">
+        {courses &&
+          courses.map(course => (
+            <div key={course.id} className="post-course-container">
+              <div className="post-course-id">{course.id}</div>
+              <div className="post-course-name">{course.name}</div>
+            </div>
+          ))}
+      </div>
+      <div className="post-info">
+        <div className="post-date">{date}</div>
+        <div className="post-comment">
+          <img src={commentIcon} alt="comment" />
+          {comment}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Post;
