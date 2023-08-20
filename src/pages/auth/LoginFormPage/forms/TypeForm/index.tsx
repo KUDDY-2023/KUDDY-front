f8ab363c0fee97efe90181ecede16c7b08d34979
Dayun <dy6578ekdbs@gmail.com>
2023-08-20 15:31:32 +0900

    🐛 type from 스타일링 충돌 해결

--- src/pages/auth/LoginFormPage/forms/TypeForm/index.tsx
+++ src/pages/auth/LoginFormPage/forms/TypeForm/index.tsx
@@ -1,16 +1,39 @@
 import "./typeform.scss";
+import { useState } from "react";
 
 export default function TypeForm() {
+  const [type, setType] = useState([true, false]);
+
+  const _handleClickTypeBtn = (type: string) => {
+    if (type === "k-buddy") {
+      setType([true, false]);
+    } else if (type === "traveler") {
+      setType([false, true]);
+    }
+  };
+
   return (
     <div className="type-form-container">
       <p className="title">Choose your user type</p>
       <div className="form-container">
-        <div className="btn-container">
-          <div id="k-buddy">K-buddy</div>
+        <div className="type-btn-container">
+          <div
+            className="inactive-type-btn"
+            onClick={() => _handleClickTypeBtn("k-buddy")}
+            id={type[0] ? "active" : ""}
+          >
+            K-buddy
+          </div>
           <p>Korean Users Providing Guided Services</p>
         </div>
-        <div className="btn-container">
-          <div id="traveler">traveler </div>
+        <div className="type-btn-container">
+          <div
+            className="inactive-type-btn"
+            onClick={() => _handleClickTypeBtn("traveler")}
+            id={type[1] ? "active" : ""}
+          >
+            traveler
+          </div>
           <p>Non-Korean Users Looking for K-buddy</p>
         </div>
       </div>
