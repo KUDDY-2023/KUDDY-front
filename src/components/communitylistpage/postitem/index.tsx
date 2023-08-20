276f22169c80d62fb10336272b91c7c7a52f1d53
Yerineee <yerin1103@ewhain.net>
2023-08-20 19:53:33 +0900

    :bug: 커뮤니티 게시판 리스트 PostItem 컴포넌트 수정

--- src/components/communitylistpage/postitem/index.tsx
+++ src/components/communitylistpage/postitem/index.tsx
@@ -4,33 +4,69 @@ import commentIcon from "@assets/community/comment.svg";
 
 const PostItem = ({ ...props }) => {
   const nav = useNavigate();
-  const clickedMenuType = new URLSearchParams(useLocation().search).get(
-    "category",
-  );
+  const category = new URLSearchParams(useLocation().search).get("category");
 
   return (
     <div
-      className="post-container"
-      onClick={() => nav(`/community/${clickedMenuType}/${props.postId}`)}
+      className="post-item-container"
+      onClick={() => nav(`/community/${category}/${props.postId}`)}
     >
-      <div className="post-title">{props.title}</div>
-      <div className="post-content">{props.content}</div>
-      {props.spotList && (
-        <div className="post-courses">
-          {props.spotList.map((spot: SpotType) => (
-            <div key={spot.id} className="post-course-container">
-              <div className="post-course-id">{spot.id}</div>
-              <div className="post-course-name">{spot.place}</div>
+      {category === "talking-board" && (
+        <div className="item-top-container">
+          <div
+            className={
+              props.filter === "Join us"
+                ? "post-filter is-join-us"
+                : "post-filter"
+            }
+          >
+            {props.filter}
+          </div>
+          {props.filter === "Join us" && (
+            <div className="join-us-container">
+              <div className="join-us-text bold-text">{props.joinDistrict}</div>
+              <div className="join-us-text">
+                · {props.joinPeople} · {props.joinDate}
+              </div>
             </div>
-          ))}
+          )}
         </div>
       )}
-      <div className="post-info">
-        <div className="post-date">{props.writeDate}</div>
-        <div className="post-comment">
-          <img src={commentIcon} alt="comment" />
-          {props.commentCnt}
+      <div className="item-bottom-container">
+        <div
+          className={
+            category === "talking-board" && props.spotList
+              ? "post-detail-container has-photo"
+              : "post-detail-container"
+          }
+        >
+          <div className="post-title">{props.title}</div>
+          <div className="post-content">{props.content}</div>
+          {props.spotList && (
+            <div className="post-spots">
+              {props.spotList.map((spot: SpotType) => (
+                <div key={spot.id} className="post-spot-container">
+                  <div className="post-spot-id">{spot.id}</div>
+                  <div className="post-spot-name">{spot.place}</div>
+                </div>
+              ))}
+            </div>
+          )}
+          <div className="post-info">
+            <div className="post-date">{props.writeDate}</div>
+            <div className="post-comment">
+              <img src={commentIcon} alt="comment" />
+              {props.commentCnt}
+            </div>
+          </div>
         </div>
+        {props.photoList && (
+          <img
+            className="post-photo"
+            alt="post-photo"
+            src={props.photoList[0].photo}
+          />
+        )}
       </div>
     </div>
   );
