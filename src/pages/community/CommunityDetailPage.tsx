12c83012f99631fa16bfc029e17fb8641c30b021
Yerineee <yerin1103@ewhain.net>
2023-08-21 01:30:29 +0900

    :lipstick: 커뮤니티 게시물 상세 페이지 퍼블리싱 작업 중

--- src/pages/community/CommunityDetailPage.tsx
+++ src/pages/community/CommunityDetailPage.tsx
@@ -1,12 +1,34 @@
 import { useParams } from "react-router-dom";
 import BackNavBar from "@components/_common/backnavbar";
+import PostDetail from "@components/communitydetailpage/postcontent";
+import {
+  ItineraryFeedbackPostData,
+  TalkingBoardPostData,
+} from "@utils/data/communityPost";
 
 const CommunityDetailPage = () => {
-  const { category } = useParams() as { category: string };
+  const { category, id } = useParams() as { category: MenuType; id: string };
+
+  // 게시물 데이터 조회 코드 추가 필요
+  let PostData: ItineraryFeedbackPost | TalkingBoardPost;
+  switch (category) {
+    case "itinerary-feedback":
+      PostData = ItineraryFeedbackPostData[parseInt(id) - 1];
+      break;
+    default:
+      PostData = TalkingBoardPostData[parseInt(id) - 1];
+  }
 
   return (
     <>
-      <BackNavBar middleTitle={category} />
+      <BackNavBar
+        middleTitle={
+          category === "itinerary-feedback"
+            ? "Itinerary Feedback"
+            : "Talking Board"
+        }
+      />
+      <PostDetail {...PostData} />
     </>
   );
 };
