8520075fddca12c51c47fdf7ec27b06b868a2457
Dayun <dy6578ekdbs@gmail.com>
2023-08-23 23:05:39 +0900

    🐛 LoginRoutes > AuthRoute 이름 변경 + 탈퇴 페이지 추가

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ import TravelDetailPage from "@pages/travel/TravelDetailPage";
 import KuddysPickPage from "@pages/travel/KuddysPickPage";
 import KuddysPickDetailPage from "@pages/travel/KuddysPickDetailPage";
 
-import LoginRoutes from "@routes/LoginRoutes";
+import AuthRoutes from "@routes/AuthRoutes";
 import CommunityRoutes from "@routes/CommunityRoutes";
 import MyRoutes from "@routes/MyRoutes";
 import ChatRoutes from "@routes/ChatRoutes";
@@ -23,7 +23,7 @@ function App() {
       <Route path="/travel/:id" element={<TravelDetailPage />} />
       <Route path="/kuddys-pick" element={<KuddysPickPage />} />
       <Route path="/kuddys-pick/:id" element={<KuddysPickDetailPage />} />
-      <Route path="/auth/*" element={<LoginRoutes />} />
+      <Route path="/auth/*" element={<AuthRoutes />} />
       <Route path="/community/*" element={<CommunityRoutes />} />
       <Route path="/my/*" element={<MyRoutes />} />
       <Route path="/chat/*" element={<ChatRoutes />} />
