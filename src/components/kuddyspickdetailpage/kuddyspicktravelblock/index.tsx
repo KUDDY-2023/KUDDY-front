bfb4d246c8ed6b8fed361fff6700694125824afe
서진 <dltjwls0529@gmail.com>
2023-08-24 19:29:57 +0900

    ✨ 커디스픽 상세 페이지 useBookmark 사용

--- src/components/kuddyspickdetailpage/kuddyspicktravelblock/index.tsx
+++ src/components/kuddyspickdetailpage/kuddyspicktravelblock/index.tsx
@@ -2,10 +2,17 @@ import "./kuddyspicktravelblock.scss";
 import { KuddysPickDetailContentType } from "@components/kuddyspickdetailpage/kuddyspickdetailmenu/index";
 import { ReactComponent as BookmarkIcon } from "@assets/travelpage/bookmark.svg";
 import { useState, useEffect } from "react";
+import useBookmark from "@utils/hooks/useBookmark";
 
 const KuddysPickTravelBlock = ({ travel }: KuddysPickDetailContentType) => {
   const { id, name, district, category, thumbnail } = travel;
-  const [isLogin, setIsLogin] = useState<boolean>(false);
+  const [isLogin, setIsLogin] = useState<boolean>(true);
+  const [isBookmarked, setIsBookmarked] = useState<boolean>(false);
+  useEffect(() => {
+    const likedList = [{ id: 93877 }, { id: 2 }].map(row => row.id);
+    setIsBookmarked(isLogin === false ? false : likedList.includes(id));
+  }, []);
+  const { state, toggle } = useBookmark(isBookmarked, id);
   return (
     <div className="kuddyspicktravelblock-wrapper">
       <div className="kuddyspickpreview-content-rect">
@@ -17,7 +24,11 @@ const KuddysPickTravelBlock = ({ travel }: KuddysPickDetailContentType) => {
           <div className="description">{`${district} · ${category}`}</div>
         </div>
         <div className="pick-icon-container">
-          <BookmarkIcon onClick={() => console.log("pick")} />
+          <BookmarkIcon
+            onClick={isLogin ? toggle : () => alert("Login to pick")}
+            stroke="var(--color-black)"
+            fill={state ? "var(--color-main-yellow)" : "var(--color-white)"}
+          />
         </div>
       </div>
     </div>
