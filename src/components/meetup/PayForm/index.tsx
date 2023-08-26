46cf45493404643fba6ac3f7cfefb3197a4c247f
Dayun <dy6578ekdbs@gmail.com>
2023-08-26 21:19:47 +0900

    💄 Pay Form

---
+++ src/components/meetup/PayForm/index.tsx
@@ -0,0 +1,11 @@
+import "./pay-form.scss";
+import { useState } from "react";
+
+export default function PayForm() {
+  return (
+    <div className="pay-form-style">
+      <p>Pay</p>
+      <div>$15</div>
+    </div>
+  );
+}
