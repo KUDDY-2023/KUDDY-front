8b5ed9c3403b5498cc84539f92a091771a958fc8
Dayun <dy6578ekdbs@gmail.com>
2023-08-26 21:19:32 +0900

    🐛 Bottom Up Modal 레이아웃 수정

--- src/components/meetup/BottomUpModalLayout/index.tsx
+++ src/components/meetup/BottomUpModalLayout/index.tsx
@@ -2,6 +2,8 @@ import "./bottomup-modal-layout.scss";
 import { useState, useEffect } from "react";
 import EventBtn from "@components/_common/eventBtn";
 import PlaceForm from "../PlaceForm";
+import TimeForm from "../TimeForm";
+import PayForm from "../PayForm";
 interface Props {
   isOpen: boolean;
 }
@@ -9,14 +11,14 @@ export default function BottomUpModalLayout({ isOpen }: Props) {
   return (
     <div className={`kuddy-container ${isOpen && "kuddy-modal-open"}`}>
       <div className="kuddy-modal">
-        <div>바</div>
+        <div id="bar"></div>
 
         <h2>Set meet up</h2>
 
         <div className="form-container">
           <PlaceForm />
-          <PlaceForm />
-          <PlaceForm />
+          <TimeForm />
+          <PayForm />
         </div>
 
         <EventBtn
