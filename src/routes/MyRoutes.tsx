41ee090437a828f4b6ef45102e52b20e0ecd4197
Yerineee <yerin1103@ewhain.net>
2023-08-27 02:30:29 +0900

    :sparkles: My appointment 페이지 라우팅 추가

--- src/routes/MyRoutes.tsx
+++ src/routes/MyRoutes.tsx
@@ -1,9 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import ReportUserPage from "@pages/my/ReportUserPage";
+import AppointmentPage from "@pages/my/AppointmentPage";
+
 export default function MyRoutes() {
   return (
     <Routes>
       <Route path="/report" element={<ReportUserPage />} />
+      <Route path="/appointment" element={<AppointmentPage />} />
     </Routes>
   );
 }
