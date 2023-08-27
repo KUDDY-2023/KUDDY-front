082a6e652109c7dd9b770ee922aab77e385c7390
Dayun <dy6578ekdbs@gmail.com>
2023-08-27 17:15:09 +0900

    💄 Date Form meterial UI

--- src/components/meetup/TimeForm/index.tsx
+++ src/components/meetup/TimeForm/index.tsx
@@ -1,12 +1,33 @@
 import "./time-form.scss";
 import { useState } from "react";
 
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { DemoContainer, DemoItem } from "@mui/x-date-pickers/internals/demo";
+import { MobileDatePicker } from "@mui/x-date-pickers/MobileDatePicker";
+import dayjs from "dayjs";
+
 export default function TimeForm() {
+  const [isOpenDate, setIsOpenDate] = useState(false);
+
   return (
-    <div className="time-form-style">
-      <p>Time</p>
-      <div className="date">2023.12.25</div>
-      <div className="time">12:00 PM</div>
-    </div>
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <div className="time-form-style">
+        <p className="title">Time</p>
+        <MobileDatePicker
+          defaultValue={dayjs("2023-08-27")}
+          sx={{
+            "& .MuiButtonBase-root-MuiPickersDay-root  ": { color: "red" },
+          }}
+        />
+
+        <div className="time">12:00 PM</div>
+      </div>
+    </LocalizationProvider>
   );
 }
+// MuiButtonBase-root
+// MuiPickersDay-root
+// Mui-selected
+// MuiPickersDay-dayWithMargin
+// css-qa7bje-MuiButtonBase-root-MuiPickersDay-root
