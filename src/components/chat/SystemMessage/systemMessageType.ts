543f31bf56c0232985cda4a54872c3f94cfd05d1
Dayun <dy6578ekdbs@gmail.com>
2023-08-28 18:07:35 +0900

    🐛 Chat2

--- src/components/chat/SystemMessage/systemMessageType.ts
+++
@@ -1,5 +0,0 @@
-export const systemMessageType = {
-  accept: { text: "Our appointment came true!", color: "yellow" },
-  reject: { text: "Our appointment was broken!", color: "red" },
-  feedback: { text: "How was the meeting?", color: "blue" },
-};
