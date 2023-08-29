57a5639b4f67bc952a5f366a38467a7f58bce68d
Dayun <dy6578ekdbs@gmail.com>
2023-08-29 15:22:20 +0900

    💄 Meet Up modal 퍼블리싱

--- src/components/MeetUp/PlaceForm/index.tsx
+++ src/components/MeetUp/PlaceForm/index.tsx
@@ -2,17 +2,31 @@ import "./place-form.scss";
 import { useState } from "react";
 
 import PlaceSearch from "../PlaceSearch";
+import useInput from "@utils/hooks/userInput";
 export default function PlaceForm() {
   const [search, setSearch] = useState(false);
+
+  const placeInput = useInput("");
+
+  const _onSelectPlace = (placeName: string) => {
+    setSearch(false); // 끄기
+    placeInput.setValue(placeName);
+    // 선택된 장소 input에 넣기
+  };
 
   return (
     <div className="place-form-style">
       {search ? (
-        <PlaceSearch />
+        <PlaceSearch onSelectPlace={_onSelectPlace} />
       ) : (
         <div className="default-form">
           <div className="title">Place</div>
-          <div className="form" onClick={() => setSearch(true)}></div>
+          <input
+            className="form"
+            onClick={() => setSearch(true)}
+            type="text"
+            {...placeInput}
+          />
         </div>
       )}
     </div>
