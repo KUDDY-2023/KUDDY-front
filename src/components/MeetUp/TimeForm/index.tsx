63d837ca23675a9a02230ffc5f0ba023959e79c0
Dayun <dy6578ekdbs@gmail.com>
2023-08-29 16:53:26 +0900

    💄 날짜 시간 선택 시 text color black으로 변경

--- src/components/MeetUp/TimeForm/index.tsx
+++ src/components/MeetUp/TimeForm/index.tsx
@@ -10,15 +10,27 @@ import { MobileTimePicker } from "@mui/x-date-pickers/MobileTimePicker";
 
 export default function TimeForm() {
   const [isOpenDate, setIsOpenDate] = useState(false);
+  const [date, setDate] = useState<any>("");
+  const [time, setTime] = useState<any>("");
+
+  let today = new Date(); // 현재 시간을 기본으로 세팅
 
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
       <div className="time-form-style">
         <div className="title">Time</div>
         <div className="forms-container">
-          <MobileDatePicker defaultValue={dayjs("2023-08-27")} />
+          <MobileDatePicker
+            defaultValue={dayjs(today)}
+            className={date !== "" ? "active-text" : ""}
+            onChange={value => setDate(value)}
+          />
 
-          <MobileTimePicker defaultValue={dayjs("2022-04-17T15:30")} />
+          <MobileTimePicker
+            defaultValue={dayjs(today)}
+            className={time !== "" ? "active-text" : ""}
+            onChange={value => setTime(value)}
+          />
         </div>
       </div>
     </LocalizationProvider>
