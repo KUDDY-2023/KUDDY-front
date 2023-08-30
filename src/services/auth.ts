700fd65ad249becbcfeafdc3da9c8439b2b24b8c
Dayun <dy6578ekdbs@gmail.com>
2023-08-30 20:14:32 +0900

    🧪 store & api 임시 코드

---
+++ src/services/auth.ts
@@ -0,0 +1,11 @@
+import axios from "axios";
+import { apiClient } from ".";
+
+// 여행객, 가이드 둘 따로임
+// 🔥 오류 관리 어떻게 할지? => hook 안에서 따로 해야할 듯
+export const CreateUserProfile = (name: IUserProfile) => {
+  const url = `/api/v1/members/profile`;
+  return apiClient.post(url, name).then(res => {
+    return res.data;
+  });
+};
--- src/utils/@types/user.d.ts
+++ src/utils/@types/user.d.ts
@@ -1,3 +1,43 @@
+// 프로필 정보 생성
+interface IUserProfile {
+  roleType: string;
+  nickname: string;
+  profileImage: string;
+  gender: string;
+  age: number;
+  temperament: string;
+  decisionMaking: string;
+  job: string;
+  wellbeing: string;
+  activitiesInvestmentTech: string;
+  careerMajor: string;
+  entertainment: string;
+  hobbiesInterests: string;
+  lifestyle: string;
+  artBeauty: string;
+  food: string;
+  sports: string;
+  nationality: string;
+  availableLanguages: [
+    {
+      languageType: string;
+      languageLevel: number;
+    },
+    {
+      languageType: string;
+      languageLevel: number;
+    },
+  ];
+  districts: [
+    {
+      areaName: string;
+    },
+    {
+      areaName: string;
+    },
+  ];
+}
+
 interface UserProfile {
   type: string;
   nickname: string;
