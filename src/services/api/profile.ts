013c25fc08b199bca5be8413e1d11c4cc99711aa
Dayun <dy6578ekdbs@gmail.com>
2023-09-02 22:22:35 +0900

    ✨ 유저 프로필 조회 api

--- src/services/api/profile.ts
+++ src/services/api/profile.ts
@@ -5,3 +5,9 @@ export const profileCheckNickname = async (nickname: string) => {
   const url = `/api/v1/members/nickname?nickname="${nickname}"`;
   return apiClient.get(url);
 };
+
+// ✅ 유저 프로필 조회
+export const profileGetProfile = async () => {
+  const url = `/api/v1/members/profile`;
+  return apiClient.get(url);
+};
