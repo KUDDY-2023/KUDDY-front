ff6a6336c0cb07ca08728702beddfd837e19537c
서진 <dltjwls0529@gmail.com>
2023-09-03 17:11:33 +0900

    💄 티켓 인증 프로세스 1차 퍼블리싱

---
+++ src/components/TicketVerificationPage/InProgressMenu/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const InProgressMenu = () => {
+  return (
+    <>
+      <div className="more-text"></div>
+      <div className="button disabled">Screening in progress</div>
+    </>
+  );
+};
+
+export default InProgressMenu;
