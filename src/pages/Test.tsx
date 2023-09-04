51b2fb6410f9682e27042a9220fe1b83af04de65
Dayun <dy6578ekdbs@gmail.com>
2023-09-04 19:18:02 +0900

    🔥 test code 삭제

--- src/pages/Test.tsx
+++ src/pages/Test.tsx
@@ -2,10 +2,5 @@ import { apiClient } from "@services/api";
 import { test } from "@services/api/auth";
 
 export default function Test() {
-  const a = process.env.REACT_APP_REACT_URL;
-  const b = process.env.REACT_APP_REST_KAKAO_API_KEY;
-  const c = process.env.REACT_APP_API_HOST;
-
-  console.log(a,b,c)
   return <div>테스트 페이지</div>;
 }
