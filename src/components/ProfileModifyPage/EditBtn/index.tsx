48b7fada12731bbf30a441ac0aa7703f577f59b5
Yerineee <yerin1103@ewhain.net>
2023-09-05 16:01:16 +0900

    :lipstick: 프로필 수정 페이지 퍼블리싱

--- src/components/ProfileModifyPage/EditBtn/index.tsx
+++ src/components/ProfileModifyPage/EditBtn/index.tsx
@@ -1,9 +1,13 @@
 import "./edit-btn.scss";
 import modifyIcon from "@assets/profile/modify.svg";
 
-const EditBtn = () => {
+type PropType = {
+  onClick: () => void;
+};
+
+const EditBtn = ({ onClick }: PropType) => {
   return (
-    <div className="profile-edit-btn">
+    <div className="profile-edit-btn" onClick={onClick}>
       <img src={modifyIcon} />
       <p>edit</p>
     </div>
