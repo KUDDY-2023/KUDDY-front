86874931371842f4722581b863a1eb0f96bead76
서진 <dltjwls0529@gmail.com>
2023-09-10 00:04:52 +0900

    ✨ 티켓 인증 프로세스 API 연결

--- src/pages/my/TicketVertificationPage/index.tsx
+++ src/pages/my/TicketVertificationPage/index.tsx
@@ -5,32 +5,51 @@ import CompletedMenu from "@components/TicketVerificationPage/CompletedMenu";
 import InProgressMenu from "@components/TicketVerificationPage/InProgressMenu";
 import UploadMenu from "@components/TicketVerificationPage/UploadMenu";
 
+import {
+  profileGetTicketInfo,
+  profilePatchTicketStatus,
+} from "@services/api/profile";
+
 export type TicketInfoType = {
+  ticketId: number;
   ticketImageUrl: string;
   ticketStatus:
-    | "NOT_YET"
-    | "SCREENING_IN_PROGRESS"
-    | "CERTIFICATION_FAILED"
+    | "NOT_SUBMITTED"
+    | "UNDER_REVIEW"
+    | "INVALID_PHOTO"
+    | "PHOTO_UNRECOGNIZABLE"
     | "CERTIFICATION_COMPLETE";
 };
 
 const TicketVerificationPage = () => {
   const [ticketInfo, setTicketInfo] = useState<TicketInfoType>({
+    ticketId: 0,
     ticketImageUrl: "",
-    ticketStatus: "NOT_YET",
+    ticketStatus: "NOT_SUBMITTED",
   });
+
+  useEffect(() => {
+    profileGetTicketInfo()
+      .then(res => {
+        console.log(res.data.data);
+        setTicketInfo(res.data.data);
+      })
+      .catch();
+  }, []);
+
+  // profilePatchTicketStatus("CERTIFICATION_COMPLETE");
+
   return (
     <>
       <BackNavBar middleTitle="Ticket verification" isShare={false} />
       <div className="ticket-verification-page-wrapper">
         {ticketInfo.ticketStatus === "CERTIFICATION_COMPLETE" ? (
           <CompletedMenu />
-        ) : ticketInfo.ticketStatus === "SCREENING_IN_PROGRESS" ? (
-          <InProgressMenu />
-        ) : ticketInfo.ticketStatus === "NOT_YET" ||
-          ticketInfo.ticketStatus === "CERTIFICATION_FAILED" ? (
+        ) : ticketInfo.ticketStatus === "UNDER_REVIEW" ? (
+          <InProgressMenu {...ticketInfo} />
+        ) : (
           <UploadMenu {...ticketInfo} />
-        ) : null}
+        )}
       </div>
     </>
   );
