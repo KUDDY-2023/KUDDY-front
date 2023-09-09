60a4b8bd9058efb9533bc09fb999c2102c103974
Dayun <dy6578ekdbs@gmail.com>
2023-09-09 19:32:50 +0900

    ✨ 채팅방 리스트 api

--- src/services/api/chat.ts
+++ src/services/api/chat.ts
@@ -1,7 +1,13 @@
 import axios from "axios";
 import { apiClient } from ".";
 
-// 메세지 저장
+// ✅ 채팅방 리스트 정보 가져오기
+export const chatRooms = async () => {
+  const url = "chat/v1/chatrooms";
+  return apiClient.get(url);
+};
+
+// ✅ 메세지 저장
 export const chatSaveMessage = async (message: ISingleMessage) => {
   const url = "https://api.kuddy.co.kr/chat/v1/chatrooms/callback";
   return axios.post(url, message);
