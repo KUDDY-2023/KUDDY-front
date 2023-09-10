8f54f96d771aa55c3bdaa27eb3df55432f66cc8a
Yerineee <yerin1103@ewhain.net>
2023-09-10 20:22:55 +0900

    :bug: 이미지뷰 수정

---
+++ src/components/_common/ImageView/index.tsx
@@ -0,0 +1,83 @@
+import "./image-view.scss";
+import "swiper/swiper.scss";
+import { useState, useEffect } from "react";
+import { useRecoilState } from "recoil";
+import { Swiper, SwiperSlide } from "swiper/react";
+import backIcon from "@assets/icon/back.svg";
+import trashBtn from "@assets/community/trash_btn.svg";
+import { joinUsPostState, othersPostState } from "@services/store/community";
+
+interface Props {
+  photoInfo: string[];
+  clickedIndex: number;
+  onClose(): void;
+  hasTrashBtn?: boolean;
+  isJoinUs?: boolean;
+}
+
+const ImageView = ({
+  photoInfo,
+  clickedIndex,
+  onClose,
+  hasTrashBtn,
+  isJoinUs,
+}: Props) => {
+  const [curIndex, setCurIndex] = useState<number>(clickedIndex);
+  const [joinUsPost, setJoinUsPost] = useRecoilState(joinUsPostState);
+  const [othersPost, setOthersPost] = useRecoilState(othersPostState);
+
+  const handleTrashBtnClick = () => {
+    if (isJoinUs) {
+      setJoinUsPost({
+        ...joinUsPost,
+        images: joinUsPost.images.filter((image, index) => index !== curIndex),
+      });
+    } else {
+      setOthersPost({
+        ...othersPost,
+        images: othersPost.images.filter((image, index) => index !== curIndex),
+      });
+    }
+  };
+
+  useEffect(() => {
+    setCurIndex(clickedIndex);
+  }, []);
+
+  return (
+    <div className="image-view-container">
+      <div className="image-view-navbar">
+        <img src={backIcon} onClick={onClose} />
+        <div className="image-index">
+          {curIndex + 1} / {photoInfo.length}
+        </div>
+      </div>
+
+      <Swiper
+        className="image-view-slide-container"
+        slidesPerView={"auto"}
+        spaceBetween={0}
+        initialSlide={clickedIndex}
+        onSlideChangeTransitionStart={swiperCore => {
+          setCurIndex(swiperCore.activeIndex);
+        }}
+      >
+        {photoInfo.map((item, index) => (
+          <SwiperSlide className="image-view-slide" key={index}>
+            <img src={item} />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+
+      {hasTrashBtn && (
+        <img
+          className="trash-btn"
+          src={trashBtn}
+          onClick={handleTrashBtnClick}
+        />
+      )}
+    </div>
+  );
+};
+
+export default ImageView;
