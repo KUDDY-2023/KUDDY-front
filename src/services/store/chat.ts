0ee87818be4d95bb16392f6d81e5d5d3b5351941
Dayun <dy6578ekdbs@gmail.com>
2023-09-13 23:42:43 +0900

    ✨ 동행 요청 메시지 필드를 관리하는 전역 atom 생성

---
+++ src/services/store/chat.ts
@@ -0,0 +1,12 @@
+import { atom, selector } from "recoil";
+
+// ⭐ 동행 요청 메세지에 필요한 필드
+export const meetUpInfoState = atom({
+  key: "meetUpInfoState",
+  default: {
+    spotContentId: null, // 장소 id
+    spotName: null, // 장소 이름
+    appointmentTime: null, // 시간 2021-11-05 13:47:13.248
+    price: null, // 가격 정수로 $
+  },
+});
