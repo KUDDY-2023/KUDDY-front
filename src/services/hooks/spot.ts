a2fb504d713fa445c1878f1694105bd3e4c1f734
서진 <dltjwls0529@gmail.com>
2023-09-28 00:13:08 +0900

    ⚡️ 장소 상세 페이지 동기 처리 및 성능 개선

--- src/services/hooks/spot.ts
+++ src/services/hooks/spot.ts
@@ -1,11 +1,59 @@
-import { useEffect } from "react";
-import { useInfiniteQuery } from "react-query";
+import { useState, useEffect } from "react";
+import { useQuery } from "react-query";
 import useInfiniteScroll from "@utils/hooks/useInfiniteScroll";
 import {
+  spotGetDetailInfo,
+  spotGetDetailNearby,
   spotGetByFilter,
   spotGetNearLocation,
   spotGetOnlyKeyWord,
 } from "@services/api/spot";
+
+// detail
+export const useDetailSpot = (id: number) => {
+  const { data, isLoading, isFetching, isError, refetch } = useQuery(
+    ["travelDetail", id],
+    () => spotGetDetailInfo(id),
+  );
+  const { data: nearbyData } = useQuery(
+    ["travelNearby", id],
+    () => spotGetDetailNearby(id, data?.data.data.mapX, data?.data.data.mapY),
+    { enabled: !!data },
+  );
+
+  const [matesPreview, setMatesPreview] = useState<string[]>();
+  const getMatesPreview = (data: any) => {
+    if (data.data.data.kuddyList.concat(data.data.data.travelerList).length > 5)
+      setMatesPreview(
+        data.data.data.kuddyList
+          .concat(data.data.data.travelerList)
+          .sort(() => Math.random() - 0.5)
+          .slice(0, 5)
+          .map((row: PickedMatesType) => row.profileImageUrl),
+      );
+    else
+      setMatesPreview(
+        data.data.data.kuddyList
+          .concat(data.data.data.travelerList)
+          .sort(() => Math.random() - 0.5)
+          .map((row: PickedMatesType) => row.profileImageUrl),
+      );
+  };
+  useEffect(() => {
+    if (!data) return;
+    getMatesPreview(data);
+  }, [data]);
+
+  return {
+    data,
+    isLoading,
+    isFetching,
+    isError,
+    nearbyData,
+    matesPreview,
+    refetch,
+  };
+};
 
 // multi filter
 export const useAllSpot = (filter: SpotGetByFilterType) => {
@@ -25,18 +73,24 @@ export const useAllSpot = (filter: SpotGetByFilterType) => {
 
 // near my location
 export const useNearLocation = (pos: Position) => {
-  const { pageLastItemRef, data, isFetching, hasNextPage, fetchNextPage } =
-    useInfiniteScroll({
-      queryKey: ["allSpot", pos],
-      initialPage: 1,
-      fetch: spotGetNearLocation,
-      fetchParams: { pos: pos },
-      onIntersect: async (entry, observer) => {
-        observer.unobserve(entry.target);
-        if (hasNextPage && !isFetching) fetchNextPage();
-      },
-    });
-  return { pageLastItemRef, hasNextPage, data, isFetching };
+  const {
+    pageLastItemRef,
+    data,
+    isFetching,
+    error,
+    hasNextPage,
+    fetchNextPage,
+  } = useInfiniteScroll({
+    queryKey: ["allSpot", pos],
+    initialPage: 1,
+    fetch: spotGetNearLocation,
+    fetchParams: { pos: pos },
+    onIntersect: async (entry, observer) => {
+      observer.unobserve(entry.target);
+      if (hasNextPage && !isFetching) fetchNextPage();
+    },
+  });
+  return { pageLastItemRef, hasNextPage, data, isFetching, error };
 };
 
 // 키워드 하나로 검색해서 장소 결과 받아오기
