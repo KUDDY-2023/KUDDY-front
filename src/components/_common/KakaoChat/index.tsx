0ef18f165bc085e8b2458fcf6e0d5d5939697336
dy6578ekdbs <dy6578ekdbs@gmail.com>
2023-10-02 19:18:57 +0900

    🐛 Kakao 객체 인식 못하는 오류

--- src/components/_common/KakaoChat/index.tsx
+++ src/components/_common/KakaoChat/index.tsx
@@ -1,12 +1,35 @@
 import "./kakao-chat.scss";
+import { useNavigate } from "react-router-dom";
+
+declare global {
+  interface Window {
+    Kakao: any;
+  }
+}
 
 export default function KakaoChat() {
+  const history = useNavigate();
+
+  const handleChatClick = () => {
+    chatChannel();
+  };
+
+  const chatChannel = () => {
+    if (window.Kakao) {
+      window.Kakao.Channel.chat({
+        channelPublicId: "_brFxdG",
+      });
+    } else {
+      console.error("Kakao SDK is not loaded.");
+    }
+  };
+
   return (
-    <a id="chat-channel-button" href="javascript:chatChannel()">
+    <div id="chat-channel-button" onClick={handleChatClick}>
       <img
         src="/images/consult_small_yellow_pc.png"
         alt="카카오톡 채널 채팅하기 버튼"
       />
-    </a>
+    </div>
   );
 }
