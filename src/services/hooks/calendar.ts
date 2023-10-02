64baf0e9653dd38e7530f8d7f840fb4ebcc4d958
Yerineee <yerin1103@ewhain.net>
2023-10-02 22:58:49 +0900

    :sparkles: 동행페이지 캘린더 일정 추가 구현

--- src/services/hooks/calendar.ts
+++ src/services/hooks/calendar.ts
@@ -1,4 +1,4 @@
-import { addCalendar } from "@services/api/calendar";
+import { addCalendar, deleteCalendar } from "@services/api/calendar";
 
 export const useAddCalendar = () => {
   const onAddCalendar = async (chatId: string) => {
@@ -11,4 +11,17 @@ export const useAddCalendar = () => {
   };
 
   return onAddCalendar;
+};
+
+export const useDeleteCalendar = () => {
+  const onDeleteCalendar = async (chatId: string) => {
+    try {
+      const res = await deleteCalendar(chatId);
+      console.log("캘린더 삭제 성공", res);
+    } catch (err) {
+      console.log("캘린더 삭제 실패", err);
+    }
+  };
+
+  return onDeleteCalendar;
 };
