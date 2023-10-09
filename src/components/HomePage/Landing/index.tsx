3118933050b6824740d5dc90845df762df14def2
서진 <dltjwls0529@gmail.com>
2023-10-09 23:02:22 +0900

    💄 홈 페이지 커서 스타일 추가

--- src/components/HomePage/Landing/index.tsx
+++ src/components/HomePage/Landing/index.tsx
@@ -1,12 +1,13 @@
 import "./landing.scss";
 import logoimg from "@assets/home/logo.png";
+import { ReactComponent as LandingText } from "@assets/home/landing_text.svg";
 
 const Landing = () => {
   return (
     <div className="landing-wrapper">
       <div className="flex">
         <img src={logoimg} />
-        <p>KUDDY</p>
+        <LandingText className="text" />
       </div>
     </div>
   );
