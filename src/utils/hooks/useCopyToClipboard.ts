bb29a29eb3ec27bcd1d6400b658d972a9624c6fd
서진 <dltjwls0529@gmail.com>
2023-10-15 19:54:20 +0900

    🐛 useCopyToClipboard path에 env 적용

--- src/utils/hooks/useCopyToClipboard.ts
+++ src/utils/hooks/useCopyToClipboard.ts
@@ -1,12 +1,18 @@
 import { successCopyLink } from "@components/_common/SweetAlert";
+import { useLocation } from "react-router-dom";
 
+// 복사 성공 alert 이후 닫아야할 모달이 있다면 closer 전달
 const useCopyToClipboard = (closer?: () => void) => {
+  const location = useLocation();
+  const URL = process.env.REACT_APP_REACT_URL + location.pathname;
   const onCopy = async () => {
     try {
-      await navigator.clipboard.writeText(window.location.href);
+      await navigator.clipboard.writeText(URL);
       successCopyLink();
       closer && closer();
-    } catch (error) {}
+    } catch (error) {
+      console.log(error);
+    }
   };
   return { onCopy };
 };
